6ab60d531f5377c4860acba84225f3f1998e4f46
Pao-1112 <pao.genshin.11@gmail.com>
2023-02-26 21:45:42 -0300

    version-1

---
+++ assets/javaScript/past_events.js
@@ -0,0 +1,17 @@
+let container = document.getElementById('card_container');
+let fragment = document.createDocumentFragment();
+let date_past = new Date(data_cards.currentDate);
+
+for (let card of data_cards.events){
+    let date_events = new Date (card.date);
+    if(date_past < date_events){
+        let div = document.createElement('div');
+        div.innerHTML = `<div class="grid d-flex gap-4 p-2"><div class="p-2 g-col-6">
+        <div class="card p-3" style="width: 20rem; height: 28rem;"><figure class="p-2"><img src=${card.image} class="card-img-top card1_img alt="cinema"></img>
+        </figure><div class="card-body"><h5 class="card-title text-center">${card.name}</h5>
+        <p class="card-text text-center p-2">${card.description}</p><div class="d-flex justify-content-between">
+        <p>$ ${card.price}</p><a href="./details.html" class="text-decoration-none text-white text-center card_buttom p-2">Ver mas</a></div></div></div></div>`;
+        fragment.appendChild(div);
+    }
+}
+container.appendChild(fragment);
