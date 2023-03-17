6f1e57e974a7798b430c0ad5595fffbd5c2ab0ea
Pao-1112 <pao.genshin.11@gmail.com>
2023-03-17 06:04:01 -0300

    version-1

--- assets/javaScript/details.js
+++ assets/javaScript/details.js
@@ -1,26 +1,36 @@
-const queryString = location.search;
+interfazData()
+async function interfazData(claves){
+  try{
+    let claves = [];
 
-const params = new URLSearchParams(queryString);
+    let rest = await fetch("https://mindhub-xj03.onrender.com/api/amazing");
+    let data = await rest.json();
 
-const id = params.get('id');
-
-const detail_card = data_cards.events.find(events => events._id == id);
-
-const div = document.getElementById('cards_details');
-div.innerHTML = `
-<div class="p-5 border border-danger-black bg-white w-50">
-  <img src="${detail_card.image}" alt="${detail_card.name}" class="card_details_img p-5">
-</div>
-<div class="p-1 border border-danger-black bg-white w-50 p-4">
-  <h3 class="card-title text-center p-2">${detail_card.name}</h3>
-  <div class="p-2">
-    <p class="card-text">Date: ${detail_card.date}</p>
-    <p class="card-text">Description: ${detail_card.description}</p>
-    <p class="card-text">Category: ${detail_card.category}</p>
-    <p class="card-text">Place: ${detail_card.place}</p>
-    <p class="card-text">Capacity: ${detail_card.capacity}</p>
-    <p class="card-text">Assistance or Estimate: ${detail_card.assistance}</p>
-    <p class="card-text"><small class="text-muted">Price: $ ${detail_card.price}</small></p>
-    <a href="./index.html" class="text-decoration-none text-white text-center card_buttom p-2 aling-items-end>Volver</a>
-  </div>
-</div>`
+    const queryString = location.search;
+    const params = new URLSearchParams(queryString);
+    const id = params.get('id');
+    const detail_card = data.events.find(events => events._id == id);
+    const div = document.getElementById('cards_details');
+    div.innerHTML = `
+    <div class="card_details_body">
+      <div class="card_details_img">
+        <figure class="card_img">
+          <img src="${detail_card.image}" alt="${detail_card.name}" class="card_img">
+        </figure>
+      </div>
+      <div class="card_main">
+        <h3 class="card_title">${detail_card.name}</h3>
+        <p class="card-text">Date: ${detail_card.date}</p>
+        <p class="card-text">Description: ${detail_card.description}</p>
+        <p class="card-text">Category: ${detail_card.category}</p>
+        <p class="card-text">Place: ${detail_card.place}</p>
+        <p class="card-text">Capacity: ${detail_card.capacity}</p>
+        ${detail_card.assistance ? `<p class= "card-text">Assistance: ${detail_card.assistance}</p>` : ''}
+        ${detail_card.estimate ? `<p class="card-text">Estimate: ${detail_card.estimate}</p>` : ''}
+        <p class="card-text"><small class="text-muted">Price ${detail_card.price}</small></p>
+      </div>
+    </div> `
+  }catch(error){
+    console.log('Im in the catch: ' + error.mesage);
+  }
+}
--- assets/javaScript/upcoming_events.js
+++ assets/javaScript/upcoming_events.js
@@ -1,37 +1,83 @@
-//visualización de las 7 card
-const container_cards = document.getElementById('card_container');
-let date_current = new Date(data_cards.currentDate);
+//visualización de las cards de forma Asincrónica
 
+const container_cards = document.getElementById('card_container');
+
+async function load(container){
+  try {
+  let respuesta = await fetch("https://mindhub-xj03.onrender.com/api/amazing")
+  let data = await respuesta.json()
+
+  let current_data = new Date(data.currentDate);
+  let array_date = data.events.filter(evento => new Date (evento.date) > current_data);
+    console.log(array_date);
+  see_cards(array_date, container);
+  const category_checkbox_container = document.getElementById('category_checkbox');
+  category_checkbox_container.appendChild(checkboxs(data.events));
+
+  let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  let inputSelected = [];
+  console.log(checkboxes);
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', ()=> {
+      inputSelected = Array.from(checkboxes).filter(checkbox => checkbox.checked).map(input => input.value);
+      console.log(inputSelected);
+      filter_all(array_date, inputSelected, text_search, container);
+    })
+  });
+
+  const search_input = document.getElementById('searchs');
+  console.log(search_input);
+  let text_search = '';
+  search_input.addEventListener('keyup', (e) => {
+    text_search = e.target.value
+    //console.log(text_search)
+    //console.log(text(text_search, data.events))
+    filter_all(array_date, inputSelected, text_search, container)
+  });
+  
+
+  }catch(error){
+    console.log('Im in the catch: ' + error.message)
+  }
+}
+load(container_cards)
+
+//visualización de las card filtradas, presente
 function see_cards(cards_list, container){
   container.innerHTML = '';
   if (cards_list.length > 0){
     let fragment = document.createDocumentFragment();
     for (let card of cards_list){
-      let date_events = new Date(card.date);
-      if(date_current > date_events){
-        let div = document.createElement('div');
-        div.innerHTML = `<div class="grid d-flex gap-4 p-2"><div class="p-2 g-col-6">
-        <div class="card p-3" style="width: 20rem; height: 28rem;"><figure class="p-2"><img src=${card.image} class="card-img-top card1_img alt="card"></img>
-        </figure><div class="card-body"><h5 class="card-title text-center">${card.name}</h5>
-        <p class="card-text text-center p-2">${card.description}</p><div class="d-flex justify-content-between">
-        <p>$ ${card.price}</p><a href="./details.html?id=${card._id}" class="text-decoration-none text-white text-center card_buttom p-2">Ver mas</a></div></div></div></div>`;
-        fragment.appendChild(div);
-      }
-      container.appendChild(fragment);
+      let div = document.createElement('div');
+      div.innerHTML = `
+      <div class="grid d-flex gap-4 p-2"><div class="p-2 g-col-6">
+        <div class="card p-3" style="width: 20rem; height: 28rem;">
+          <figure class="p-2">
+            <img src=${card.image} class="card-img-top card1_img alt="cards"></img>
+          </figure>
+          <div class="card-body"><h5 class="card-title text-center">${card.name}</h5>
+            <p class="card-text text-center p-2">${card.description}</p>
+            <div class="d-flex justify-content-between">
+              <p>$ ${card.price}</p>
+              <a href="./details.html?id=${card._id}" class="text-decoration-none text-white text-center card_buttom p-2">Ver mas</a>
+            </div>
+          </div>
+        </div>
+      </div>`;
+      fragment.appendChild(div);
     }
+    container.appendChild(fragment);
   }
   else {
     let div = document.createElement('div');
-    div.innerHTML = `<h3>El evento no existe. Realice la búsqueda nuevamente</h3>`
+    div.innerHTML = `
+      <p>The event does not exist</p>
+      <p>Please search again</p>`
     container.appendChild(div);
   }
 }
-see_cards(data_cards.events, container_cards);
 
 //Filtrar categorias para que no se repitan y las guardo en un array
-const category_checkbox_container = document.getElementById('category_checkbox');
-category_checkbox_container.appendChild(checkboxs(data_cards.events));
-
 function checkboxs(array){
   let array_categories = [];
   for (let elements of array){
@@ -41,9 +87,8 @@ function checkboxs(array){
   let categori = array_categories.filter((item, index) => {
     return array_categories.indexOf(item) === index;
   })
-
-//Genero un div por cada category ya filtrado, con su 
-//respectivo checkbox y lo muesto con sus estilos
+  //Genero un div por cada category ya filtrado, con su
+  //respectivo checkbox y lo muesto con sus estilos
   let fragment_checkbox = document.createDocumentFragment();
   for(let catego of categori){
     let div = document.createElement('div');
@@ -54,42 +99,16 @@ function checkboxs(array){
       </div>`
     fragment_checkbox.appendChild(div);
   }
-return fragment_checkbox;
+  return fragment_checkbox;
 }
 
-//Escucho los checkbox
-let checkboxes = document.querySelectorAll('input[type="checkbox"]')
-console.log(checkboxes);
-
-checkboxes.forEach(checkbox => { 
-  checkbox.addEventListener('change', verifyCheckbox )
-});
-
-//capturo cada uno de los checkbox escuchados y los muestro
-let inputSelected = [];
-
-function verifyCheckbox(){
-  inputSelected = Array.from(checkboxes).filter(checkbox => checkbox.checked).map(input => input.value);
-  console.log(inputSelected);
-  filter_all(data_cards.events)
-}
-
+//Filtro las categorías
 function array_filter(array_string, cards_list){
   if(array_string.length === 0) return cards_list;
   return cards_list.filter(elements => array_string.includes(elements.category));
 }
 
-//serchs        
-const search_input = document.getElementById('searchs');
-console.log(search_input);
-let text_search = '';
-search_input.addEventListener('keyup', (e) => {
-  text_search = e.target.value
-  //console.log(text_search)
-  //console.log(text(text_search, data_cards.events))
-  filter_all(data_cards.events)// paso como parametro "data.events"
-});
-
+//serch 
 function text(search, list_card){
   if(search == "") return list_card;
   let new_arrays = list_card.filter(elements => elements.name.toLowerCase().includes(search.toLowerCase().trim()));
@@ -97,11 +116,8 @@ function text(search, list_card){
 }
 
 //Filtros Cruzados
-let select_checked = [];
-let input_text = '';
-function filter_all(array) {
-  
-  let cardsCheckesFiltered = array_filter (inputSelected, array); 
-  let cardsInputFiltered = text (text_search, cardsCheckesFiltered);
-  see_cards(cardsInputFiltered, container_cards)
+function filter_all(array, input_select, textSerch, contenedor) {
+  let cardsCheckesFiltered = array_filter (input_select, array);
+  let cardsInputFiltered = text (textSerch, cardsCheckesFiltered);
+  see_cards(cardsInputFiltered, contenedor);
 }
